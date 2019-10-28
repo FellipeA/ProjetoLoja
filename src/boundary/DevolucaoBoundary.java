79b5568cf2be1a04741aa65a4637021fbf72c93d
Rafael Antonio <borgesrafael935@gmail.com>
2019-10-28 13:54:41 -0300

    ....

--- src/boundary/DevolucaoBoundary.java
+++ src/boundary/DevolucaoBoundary.java
@@ -57,6 +57,7 @@ public class DevolucaoBoundary extends Application
 		txtGarantia.getItems().setAll(TipoGarantia.values());
 		grp.add(new Label("Razão: "), 0, 4);
 		grp.add(txtRazao, 1, 4);
+		
 		stage.setScene(Scn);
 		stage.show();
 	}
