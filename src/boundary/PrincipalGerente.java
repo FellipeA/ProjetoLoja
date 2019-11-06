50d1b1641670c94e84f29bf480a98c9fa934d287
Rafael Antonio <borgesrafael935@gmail.com>
2019-11-05 22:51:09 -0200

    .

--- src/boundary/PrincipalGerente.java
+++ src/boundary/PrincipalGerente.java
@@ -12,12 +12,18 @@ import javafx.stage.Stage;
 
 public class PrincipalGerente extends Application
 {
-	private Button VB = new Button(" Visualizar Balanço ");
+	private Button VB = new Button("  Visualizar Balanço  ");
 	private Button MF = new Button(" Manter Funcionário ");
 	private Button MD = new Button("  Manter Devolução  ");
+	private Button btnProduto = new Button("    Manter Produto    ");
+	private Button btnVenda = new Button("      Manter Venda     ");
+	private Button btnCliente = new Button("     Manter Cliente     ");
 	private Tooltip VBT = new Tooltip(" Visualizar Balanço ");
 	private Tooltip MFT = new Tooltip(" Manter Funcionário ");
 	private Tooltip MDT = new Tooltip("  Manter Devolução  ");
+	private Tooltip dicaProduto = new Tooltip(" Manter Produto ");
+	private Tooltip dicaVenda = new Tooltip(" Manter Funcionário ");
+	private Tooltip dicaCliente = new Tooltip("  Manter Devolução  ");
 	public static void main(String[] args) 
 	{
 		Application.launch(args);
@@ -30,16 +36,27 @@ public class PrincipalGerente extends Application
 		stage.getIcons().add(new Image(PrincipalGerente.class.getResourceAsStream("icon2.png")));
 		VBox prGerente = new VBox();
 		BorderPane brp = new BorderPane();
-		Scene Scn = new Scene(brp, 250, 250);
+		Scene Scn = new Scene(brp, 350, 350);
 		MF.setTooltip(MFT);
 		MD.setTooltip(MDT);
 		VB.setTooltip(VBT);
+		btnProduto.setTooltip(dicaProduto);
+		btnVenda.setTooltip(dicaVenda);
+		btnCliente.setTooltip(dicaVenda);
 		DevolucaoBoundary bd = new DevolucaoBoundary();
 		MD.setOnAction(e -> 
 		{
 			try 
 			{
 				bd.start(new Stage());
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		});
+		FuncionarioBoundary fb = new FuncionarioBoundary();
+		MF.setOnAction(e -> {
+			try {
+				fb.start(new Stage());
 			} catch (Exception e1) {
 				e1.printStackTrace();
 			}
@@ -52,8 +69,24 @@ public class PrincipalGerente extends Application
 				e1.printStackTrace();
 			}
 		});
+		HardwareBoundary hw = new HardwareBoundary();
+		btnProduto.setOnAction(e -> {
+			try {
+				hw.start(new Stage());
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		});
+		ClienteBoundary cb = new ClienteBoundary();
+		btnCliente.setOnAction(e -> {
+			try {
+				cb.start(new Stage());
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		});
 		
-		prGerente.getChildren().addAll(VB,MF,MD);
+		prGerente.getChildren().addAll(VB,MF, MD, btnProduto, btnCliente, btnVenda);
 		prGerente.setSpacing(20);
 		prGerente.setAlignment(Pos.CENTER);
 		brp.setCenter(prGerente);
