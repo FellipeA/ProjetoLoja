3f06ba4a5c647aa9f78ad3a6037873279ac5635a
WINDOWS ULTIMATE <WINDOWS ULTIMATE@DESKTOP-89AR9BS>
2019-11-28 01:01:50 -0200

    DAO E CONTROL

--- src/control/DevolucaoControl.java
+++ src/control/DevolucaoControl.java
@@ -1,18 +1,74 @@
 package control;
 
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+import dao.DaoDevolucao;
+import dao.DaoDevolucaoI;
+import dao.DaoException;
+import dao.DaoHardware;
+import dao.DaoHardwareI;
 import entity.Devolucao;
+import entity.Hardware;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class DevolucaoControl 
 {
-	private ObservableList<Devolucao> lista = FXCollections.observableArrayList();
-	public void adicionar(Devolucao d) 
-	{ 
-		getLista().add(d);
+	private ObservableList<Devolucao> listaDevolucao = FXCollections.observableArrayList();
+	
+	public void adicionar(Devolucao d) { 
+		try {
+			DaoDevolucaoI IDevolucao = new DaoDevolucao();
+			IDevolucao.adicionarDevolucao(d);
+			listaDevolucao.add(d);
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (DaoException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 	}
-	public ObservableList<Devolucao> getLista() 
-	{
-		return lista;
+	
+	public Devolucao pesquisarPorNome(String Cliente) { 
+		Devolucao d = new Devolucao();
+		try {
+			DaoDevolucaoI iDevolucao = new DaoDevolucao();
+			d = iDevolucao.pesquisarDevolucao(Cliente);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (DaoException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return d;
+	}
+
+	public void remover(Hardware h) { 
+		try {
+			DaoHardwareI iHardware = new DaoHardware();
+			iHardware.removerHardware(h);
+		} catch (DaoException | ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public List<Devolucao> getListaDevolucao() {
+		List<Devolucao> devolucoes = new LinkedList();
+		try {
+			DaoDevolucaoI iDevolucao = new DaoDevolucao();
+			devolucoes = iDevolucao.getDevolucao();
+			
+		} catch (ClassNotFoundException | DaoException | SQLException e) {
+			e.printStackTrace();
+		}
+		return devolucoes;
+		//Pronto
+
 	}
 }
